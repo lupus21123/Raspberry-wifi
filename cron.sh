ac55dc874f6ebec3c91ec25de80cf6fcfdb504bb
lupus21123 <argenteuslupus21123@kentech.ac.kr>
2025-01-23 07:55:02 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo raspi-config nonint do_wifi_country KR
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/sudo /usr/sbin/rfkill unblock wlan && /usr/bin/sudo /usr/sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf && /usr/bin/sudo /sbin/dhclient wlan0") | crontab -
 sudo rm -rf "$(pwd)"
