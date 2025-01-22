d1f7f2549bfdae1821f8794fdaf89ca1a0ba8d6d
lupus21123 <argenteuslupus21123@kentech.ac.kr>
2025-01-23 07:39:41 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(crontab -l 2>/dev/null; echo "@reboot sudo rfkill unblock wlan && sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf && sudo dhclient wlan0") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /usr/bin/sudo /usr/sbin/rfkill unblock wlan && /usr/bin/sudo /usr/sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf && /usr/bin/sudo /sbin/dhclient wlan0") | crontab -
 sudo rm -rf "$(pwd)"
